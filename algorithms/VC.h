ecf409daa4b3dae172ebc9f1782e5783d286a366
greg <go2432@wayne.edu>
2022-11-17 03:05:56 -0600

    at least it terminates

--- algorithms/VC.h
+++ algorithms/VC.h
@@ -36,6 +36,18 @@ struct VC_Vertex_F {
   }
 };
 
+template <typename T, typename SM> 
+struct VC_Should_Be_Deleted_F {
+  T *inCover;
+  const SM &G;
+
+  explicit VC_Should_Be_Deleted_F(T *_inCover, const SM &_G) : inCover(_inCover), G(_G) {}
+  inline bool operator()(uintE i) {
+    inCover[i] = G.getDegree(i) > 0;
+    return G.getDegree(i) > 0;
+  }
+};
+
 template <typename T, typename SM> struct VC_ROUND_1_F {
   T *inCover;
   // vertex* V;
@@ -92,7 +104,7 @@ template <typename T, typename SM> struct VC_ROUND_2_F {
   edgesToRemove(_edgesToRemove),
   G(_G)  {}
   inline bool update(uint32_t s, uint32_t d) { // Update
-    if (inCover[d]) {
+    if (inCover[d] && G.getDegree(d)>0) {
       solution[d] = 1;
       if (*numToEdgesRemove + 2 < *maxNumToEdgesRemove){
         edgesToRemove[*numToEdgesRemove] = std::tuple<el_t, el_t>{s, d};
@@ -100,28 +112,32 @@ template <typename T, typename SM> struct VC_ROUND_2_F {
         edgesToRemove[*numToEdgesRemove] = std::tuple<el_t, el_t>{d, s};
         ++(*numToEdgesRemove);
       } else {
-        printf("Failed to remove edge.  Batch size exceeded!");
-        exit(1);
+        //printf("Failed to remove edge.  Batch size exceeded!");
+        //exit(1);
       }
-      return false;
+      return true;
+      //return false;
     }
-    return true;
+    return false;
+    //return true;
   }
   inline bool updateAtomic(uint32_t s, uint32_t d) { // atomic version of Update
-    if (inCover[d]) {
-      solution[d] = 1;
+    if (inCover[s] && G.getDegree(s)>0) {
+      solution[s] = 1;
       if (*numToEdgesRemove + 2 < *maxNumToEdgesRemove){
         uint32_t edgeIndex = __sync_add_and_fetch(numToEdgesRemove, 1);
         edgesToRemove[edgeIndex] = std::tuple<el_t, el_t>{s, d};
         edgeIndex = __sync_add_and_fetch(numToEdgesRemove, 1);
         edgesToRemove[edgeIndex] = std::tuple<el_t, el_t>{d, s};
       } else {
-        printf("Failed to remove edge.  Batch size exceeded!");
-        exit(1);
+        //printf("Failed to remove edge.  Batch size exceeded!");
+        //exit(1);
       }
-      return false;
+      return true;
+      //return false;
     }
-    return true;
+    return false;
+    //return true;
   }
   // cond function checks if vertex has non-zero degree
   inline bool cond(uint32_t d) {
@@ -160,26 +176,41 @@ template <typename SM> int32_t *VC_with_edge_map(SM &G) {
   // Sparse
   VertexSubset remaining_vertices =
       VertexSubset(0, n, true); // initial set contains all vertices
+  VertexSubset vertices_to_delete;
   while (remaining_vertices.non_empty()) { // loop until set of remaining vertices is empty
     // Read phase
     // Set inCover array (I)
     G.edgeMap(remaining_vertices, VC_ROUND_1_F(inCover, G), false, 20);
     //remaining_vertices.print();
-    VertexSubset remaining_vertices_I = G.edgeMap(remaining_vertices, VC_ROUND_2_F(inCover, solution, edgesToRemove, &b_used, &b_size, G), true, 20);
-    //remaining_vertices_I.print();
+    bool continueOn = false;
+    do {
+      b_used = 0;
+      // returns vertices to delete
+      vertices_to_delete = G.edgeMap(remaining_vertices, VC_ROUND_2_F(inCover, solution, edgesToRemove, &b_used, &b_size, G), true, 20);
+      printf("VERTS 2 DEL\n");
+      vertices_to_delete.print();
+      printf("non_empty : %d\n", vertices_to_delete.non_empty());
+      printf("batch removes : %d\n", b_used);
+      printf("inCover\n");
+      for(int64_t i = 0; i < n; i++) { printf("%u ", inCover[i]); }
+      printf("\n");
+      // Write phase
+      G.remove_batch(edgesToRemove, b_used);
+    } while(vertices_to_delete.non_empty());
     //printf("B4\n");
     //for(int64_t i = 0; i < n; i++) { printf("%u ", G.getDegree(i)); }
     //printf("\n");
-    // Write phase
-    G.remove_batch(edgesToRemove, b_used);
     // remove degree zero vertices
     //printf("after\n");
     //for(int64_t i = 0; i < n; i++) { printf("%u ", G.getDegree(i)); }
     //printf("\n");
-    VertexSubset nonzero_degree_remaining_vertices = G.vertexMap(remaining_vertices_I, VC_Vertex_F(inCover, G), true); // mark visited
+    VertexSubset nonzero_degree_remaining_vertices = G.vertexMap(remaining_vertices, VC_Vertex_F(inCover, G), true); // mark visited
     remaining_vertices = nonzero_degree_remaining_vertices;
-    //remaining_vertices.print();
-    b_used = 0;
+    printf("REM VERTS\n");
+    remaining_vertices.print();
+    printf("after\n");
+    for(int64_t i = 0; i < n; i++) { printf("%u ", G.getDegree(i)); }
+    printf("\n");
   }
   remaining_vertices.del();
   free(inCover);
