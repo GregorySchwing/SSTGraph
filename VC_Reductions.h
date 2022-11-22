cc22d0338f645d0abd2b3766c2771a66ba0bed05
greg <go2432@wayne.edu>
2022-11-21 20:42:57 -0600

    off by one frm max vertex approx mvc

--- VC_Reductions.h
+++ VC_Reductions.h
@@ -66,6 +66,107 @@ struct SET_POSSIBLE_TRIANGLES_F {
   }
 };
 
+template <typename T, typename SM> 
+struct SET_LEAVES_1_F {
+  T *isLeaf;
+  const SM &G;
+
+  explicit SET_LEAVES_1_F(T *_isLeaf, const SM &_G) : isLeaf(_isLeaf), G(_G) {}
+  inline bool operator()(uintE i) {
+    isLeaf[i] = G.getDegree(i) == 1;
+    return isLeaf[i];
+  }
+};
+
+struct BF_LEAVES_F {
+  int *isLeaf;
+  int *inCover;
+  BF_LEAVES_F(intE *_isLeaf, int *_inCover)
+      : isLeaf(_isLeaf), inCover(_inCover) {}
+  // Update ShortestPathLen if found a shorter path
+  inline bool update(uintE s, uintE d) {
+    if (isLeaf[s] > isLeaf[d]) {
+      if (inCover[d] == 0) {
+        inCover[d] = 1;
+        return 1;
+      }
+    } else if (isLeaf[s] && isLeaf[d] && h(s) < h(d)){
+        if (inCover[d] == 0) {
+        inCover[d] = 1;
+        return 1;
+      }
+    }
+    return 0;
+  }
+  inline bool updateAtomic(uintE s, uintE d) { // atomic Update
+    //intE newDist = isLeaf[s] + edgeLen;
+    return (CAS(&inCover[d], 0, 1));
+  }
+  inline bool cond([[maybe_unused]] uintE s) { return true; }
+};
+
+template <typename T, typename SM> struct SET_LEAVES_2_F {
+  T *inCover;
+  const T *isLeaf;
+  // vertex* V;
+  // PR_F(double* _p_curr, double* _p_next, vertex* _V) :
+  const SM &G;
+  SET_LEAVES_2_F(const T *_isLeaf, T *_inCover, const SM &_G) : isLeaf(_isLeaf), inCover(_inCover), G(_G)  {}
+
+  /*
+    In dense mode, EdgeMap loops over all vertices and
+    looks at incoming edges to see if the source is part of the
+    vertex set. This does not require locking because each vertex
+    only updates itself and is preferred when the vertex set is large.
+
+    I am the destination.  I only update inCover[d].
+    Degrees are const in this class.
+
+  */
+
+  inline bool update(uint32_t s, uint32_t d) { // Update
+    int bothLeaves = isLeaf[s] && isLeaf[d];
+    if (bothLeaves)
+      printf("isLeaf[%d] %d isLeaf[%d] %d\n",s,isLeaf[s],d,isLeaf[d]);
+
+    if (isLeaf[s] > isLeaf[d]) {
+      printf("isLeaf[%d] %d isLeaf[%d] %d\n",s,isLeaf[s],d,isLeaf[d]);
+      exit(1);
+      inCover[d] |= 1;
+      return false;
+    }
+  }
+  /*
+    In sparse mode, EdgeMap
+    iterates over the outgoing edges of each vertex in the subset
+    and updates the destination vertex for each edge. Because it is
+    run in parallel, synchronization must be used when accessing
+    the destination vertex data.
+
+    I am the source.  I update inCover[d] using synchronization.
+    Degrees are const in this class.
+
+  */
+  inline bool updateAtomic(uint32_t s, uint32_t d) { // atomic version of Update
+  exit(1);
+    if (G.getDegree(s) == 1){
+      if (G.getDegree(s) == G.getDegree(d)){
+        __sync_fetch_and_and(&inCover[d], h(s) < h(d));
+        return false;
+      } else {
+        __sync_fetch_and_or(&inCover[d], 1);
+        return false;
+      }
+    }
+  }
+  // cond function checks if vertex in remaining vertices set has non-zero degree
+  inline bool cond(uint32_t d) {
+    return true;
+    //return G.getDegree(d) > 0;
+  }
+};
+
+
 template <typename T, typename SM> struct LEAF_REDUCTION_RULE_F {
   T *inCover;
   T *solution;
@@ -85,18 +186,18 @@ template <typename T, typename SM> struct LEAF_REDUCTION_RULE_F {
   G(_G)  {}
   inline bool update(uint32_t s, uint32_t d) { // Update
     bool deletedEdge = false;
-    // I'm a leaf
-    if (inCover[d]){
+    // You're a leaf
+    if (inCover[s]){
       //printf("LEAF %u %"PRIu32"\n", d, G.getDegree(d));
-      // and you're a leaf
-      if (inCover[s]){
+      // and I'm a leaf
+      if (inCover[d]){
         //printf("DOUBLE LEAF %u %u (%"PRIu32") (%"PRIu32")\n", s, d, G.getDegree(s), G.getDegree(d));
         // tiebreak
         if (h(s) < h(d)){
           //printf("SINCE DOUBLE LEAF %u %u (%"PRIu32") (%"PRIu32") only add %u\n", s, d, G.getDegree(s), G.getDegree(d), d);
           printf("DELETE LEAF %d %d (%d) (%d)\n",d, s, G.getDegree(d), G.getDegree(s));
 
-          solution[d] = 1;
+          solution[s] = 1;
           if (G.has(s,d)){
             uint32_t edgeIndex = __sync_fetch_and_add(numToEdgesRemove, 1);
             if (edgeIndex < *maxNumToEdgesRemove) 
@@ -111,7 +212,7 @@ template <typename T, typename SM> struct LEAF_REDUCTION_RULE_F {
           }
         }          
       } else {
-        solution[s] = 1;
+        solution[d] = 1;
         printf("DELETE LEAF %d %d (%d) (%d)\n",s,d, G.getDegree(s), G.getDegree(d));
         if (G.has(s,d)){
           uint32_t edgeIndex = __sync_fetch_and_add(numToEdgesRemove, 1);
@@ -127,6 +228,7 @@ template <typename T, typename SM> struct LEAF_REDUCTION_RULE_F {
         }        
       }
     }
+
     return deletedEdge;
   }
   inline bool updateAtomic(uint32_t s, uint32_t d) { // atomic version of Update
@@ -359,9 +461,11 @@ template <typename SM> int32_t VC_Reductions::RemoveMaxApproximateMVC(SM &approx
   int32_t b_used = 0; 
   std::tuple<el_t, el_t> *edgesToRemove = (std::tuple<el_t, el_t> *)malloc(b_size * sizeof(std::tuple<el_t, el_t>));
 
+  int32_t *isLeaf = (int32_t *)malloc(n * sizeof(int32_t));
   int32_t *inCover = (int32_t *)malloc(n * sizeof(int32_t));
   int32_t *solution = (int32_t *)malloc(n * sizeof(int32_t));
 
+  parallel_for(int64_t i = 0; i < n; i++) { isLeaf[i] = 0; }
   parallel_for(int64_t i = 0; i < n; i++) { inCover[i] = 0; }
   parallel_for(int64_t i = 0; i < n; i++) { solution[i] = 0; }
 
@@ -383,21 +487,36 @@ template <typename SM> int32_t VC_Reductions::RemoveMaxApproximateMVC(SM &approx
       leafHasChanged = false;
       triangleHasChanged = false;
 
-      VertexSubset leaves = approxGraph.vertexMap(remaining_vertices, SET_LEAVES_F(inCover, approxGraph), true); // mark visited
-      //printf("NumLeaves %u\n",leaves.get_n());
+      printf("Degrees\n");
+      for (unsigned int i = 0; i < n; i++)
+        printf("%d ", approxGraph.getDegree(i));
+      printf("\n");
+      approxGraph.vertexMap(remaining_vertices, SET_LEAVES_1_F(isLeaf, approxGraph), false); // mark visited
+      VertexSubset leaves = approxGraph.edgeMap(remaining_vertices, BF_LEAVES_F(isLeaf, inCover), true, 20);
+      printf("IsLeaf\n");
+      for (unsigned int i = 0; i < n; i++)
+        printf("%d ", isLeaf[i]);
+      printf("\n");
+      printf("In cover\n");
+      for (unsigned int i = 0; i < n; i++)
+        printf("%d ", inCover[i]);
+      printf("\n");
+      printf("NumLeaves %u\n",leaves.get_n());
 			while (leaves.non_empty()) { // loop until no leaves remain
-        b_used = 0; 
-        for (unsigned int i = 0; i < n; i++)
-          printf("%d ", approxGraph.getDegree(i));
-        printf("\n");
-        vertices_to_delete = approxGraph.edgeMap(remaining_vertices, LEAF_REDUCTION_RULE_F(inCover, solution, edgesToRemove, &b_used, &b_size, approxGraph), true, 20);
-        if (vertices_to_delete.non_empty()){
-          leafHasChanged = true;
-        }
+        b_used = 0;
+        //__sync_fetch_and_and(&b_used, 0);
+        // returns vertices to delete
+        vertices_to_delete = approxGraph.edgeMap(remaining_vertices, DELETE_VERTEX_F(inCover, solution, edgesToRemove, &b_used, &b_size, approxGraph), true, 20);
         // Write phase
         approxGraph.remove_batch(edgesToRemove, min(b_used, b_size));
-        leaves = approxGraph.vertexMap(remaining_vertices, SET_LEAVES_F(inCover, approxGraph), true); // mark visited
+        //if(percentage < 95.0)
+        //  progressBar.printIterationBar(vertices_to_delete.get_n());
+        approxGraph.vertexMap(remaining_vertices, SET_LEAVES_1_F(isLeaf, approxGraph), false); // mark visited
+        leaves = approxGraph.edgeMap(remaining_vertices, BF_LEAVES_F(isLeaf, inCover), true, 20);
       }
+      
+      //parallel_for(int64_t i = 0; i < n; i++) { inCover[i] = 0; }
+
       VertexSubset triangles = approxGraph.edgeMap(remaining_vertices, SET_TRIANGLES_F(inCover, solution, edgesToRemove, &b_used, &b_size, approxGraph), true, 20);
 			while (triangles.non_empty()) { // loop until no leaves remain
         b_used = 0; 
