f5577db4ce3846b8eb1f69f999abac701ad7345d
greg <go2432@wayne.edu>
2022-12-26 13:16:56 -0600

    instead of encapsulating the predeessors in each vertex.  Can I use a LL?

--- Chen/MaximumMatching.h
+++ Chen/MaximumMatching.h
@@ -4,6 +4,106 @@
 #include "Match.h"
 #include <limits>
 
+template <typename T, typename SM> 
+struct GET_LEVEL_I_F {
+  int64_t i;
+  T *evenlevel;
+  T *oddlevel;
+  const SM &G;
+
+  explicit GET_LEVEL_I_F(int64_t _i, T *_evenlevel, T *_oddlevel, const SM &_G) : i(_i), evenlevel(_evenlevel), oddlevel(_oddlevel), G(_G) {}
+  inline bool operator()(uintE v) {
+    return min(evenlevel[v], oddlevel[v]) == i;
+  }
+};
+
+
+
+template <typename T, typename SM> struct EVEN_LEVEL_F {
+  T * evenlevel;
+  T * oddlevel;
+  T * bridges;
+  T * predecessors;
+  T * anomalies;
+  bool *unvisited_v;
+  bool *unused_e;
+  bool *unvisited_e;
+  int64_t i;
+  const SM &G;
+  EVEN_LEVEL_F(T * _evenlevel,
+        T * _oddlevel,
+        T * _bridges,
+        T * _predecessors,
+        T * _anomalies,
+        bool *_unvisited_v,
+        bool *_unused_e,
+        bool *_unvisited_e,
+        int64_t _i,
+        const SM &_G) :
+  evenlevel(_evenlevel),
+  oddlevel(_oddlevel),
+  bridges(_bridges),
+  predecessors(_predecessors),
+  anomalies(_anomalies),
+  unvisited_v(_unvisited_v),
+  unused_e(_unused_e),
+  unvisited_e(_unvisited_e),
+  i(_i),
+  G(_G)  {}
+
+  /*
+    In dense mode, EdgeMap loops over all vertices and
+    looks at incoming edges to see if the source is part of the
+    vertex set. This does not require locking because each vertex
+    only updates itself and is preferred when the vertex set is large.
+
+    I am the destination.  I only update inCover[d].
+    Degrees are const in this class.
+
+  */
+
+  inline bool update(uint32_t s, uint32_t d) { // Update
+    if (evenlevel[s] < std::numeric_limits<int32_t>::max()){
+      int64_t temp = (evenlevel[s] + evenlevel[d])/2;
+      //bridges[temp] = ???
+    } else {
+      if (oddlevel[s] == std::numeric_limits<int32_t>::max()){
+        oddlevel[s] = i + 1;
+      }
+      if (oddlevel[s] == i + 1){
+        //predecessors[s] = ???
+      }
+      if (oddlevel[s] < i){
+        //anomalies[s] = ???
+      }
+    } 
+    return false;
+  }
+  /*
+    In sparse mode, EdgeMap
+    iterates over the outgoing edges of each vertex in the subset
+    and updates the destination vertex for each edge. Because it_i is
+    run in parallel, synchronization must be used when accessing
+    the destination vertex data.
+
+    I am the source.  I update inCover[d] using synchronization.
+    Degrees are const in this class.
+
+  */
+  inline bool updateAtomic(uint32_t s, uint32_t d) { // atomic version of Update
+    //if (performStruction[s]){
+    //  __sync_fetch_and_and(&numStructionNeighbors[d], performStruction[s]);
+    //}
+    return false;
+  }
+  // cond function checks if vertex in remaining vertices set has non-zero degree
+  inline bool cond(uint32_t d) {
+    return true;
+    //return G.getDegree(d) > 0;
+  }
+};
+
+
 template <typename SM> 
 class MaximumMatcher {
   public:
@@ -51,8 +151,18 @@ class MaximumMatcher {
 
     int Search() {
       // 1
-      for(int64_t i = 0; i < n; i++) { 
+      for(int64_t i = 0; i < n; i++)
         if (match[i] < 4) evenlevel[i] = 0;
+
+      // 2 
+      VertexSubset level_i_vertices = G.vertexMap(remainingVertices, GET_LEVEL_I_F(i, evenlevel, oddlevel, G), true); // mark visited
+      if (!level_i_vertices.non_empty())
+        return 0;
+
+      // 3 
+      if (i % 2 == 0){
+
+      }
     }
 
     private:
